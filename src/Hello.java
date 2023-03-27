5164d590aa69eeaa461764c3a58a149af65e28e0
golddrone7 <95079688+golddrone7@users.noreply.github.com>
2023-03-27 10:38:36 +0900

    initial commit

---
+++ src/Hello.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+// alt + 1 (메뉴 포커스, 접힘)
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("안녕하세요!!"); // sout , 컨트롤 + shfit + f10 실행!!
+
+        int i = 100;// 컨트롤 + 알트 + v  [무조건 외우기 ]
+        String name = "김철수"; // 타입 알아서 잡음
+        double rn = Math.random();
+
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
